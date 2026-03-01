037ac4e6955b48d960491df9d5de3885d526463a
saleena <saleenagul35@gmail.com>
2026-03-01 13:33:36 +0500

    first commit

--- app/calculator/page.js
+++ app/calculator/page.js
@@ -91,8 +91,8 @@ const Calculator = () => {
     return (
         <>
             <section className="flex justify-center items-center ">
-                <div className={`${theme ? styles.backgroundD: styles.backgroundL }  w-87 rounded-[39px]  h-screen`} >
-                    <button className={`m-5 p-3  rounded-4xl cursor-pointer ${theme ? styles.colorD : styles.colorL}  `} onClick={ChangeTheme}><b>Dark</b></button>
+                <div className={`${!theme ? styles.backgroundD: styles.backgroundL }  w-87 rounded-[39px]  h-screen`} >
+                    <button className={`m-5 p-3  rounded-4xl cursor-pointer ${!theme ? styles.colorD : styles.colorL}  `} onClick={ChangeTheme}><b>Dark</b></button>
                     <div className={`relative top-30  right-3  h-2/5  pl-7 pr-7`}>
                         <span className="m-1.5"><b>{previous}</b></span>
                         <span > <b>{operator}</b></span>
@@ -101,46 +101,46 @@ const Calculator = () => {
                     </div>
                     <div className={`fixed bottom-0 h-3/5  pl-7 pr-7`}>
                         <div className={`flex gap-5  mb-4 text-center`}>
-                            <div className={`pt-1.5 pb-1.5 pl-6 pr-6 ${theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("e")}>e</div>
-                            <div className={`pt-1.5 pb-1.5 pl-6 pr-6 ${theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("log")}>log</div>
-                            <div className={`pt-1.5 pb-1.5 pl-5 pr-5 ${theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("sin")}>sin</div>
-                            <div className={`pt-1.5 pb-1.5 pl-6 pr-6  ${theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("√")}>√</div>
+                            <div className={`pt-1.5 pb-1.5 pl-6 pr-6 ${!theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("e")}>e</div>
+                            <div className={`pt-1.5 pb-1.5 pl-6 pr-6 ${!theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("log")}>log</div>
+                            <div className={`pt-1.5 pb-1.5 pl-5 pr-5 ${!theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("sin")}>sin</div>
+                            <div className={`pt-1.5 pb-1.5 pl-6 pr-6  ${!theme ? styles.colorD : styles.colorL} rounded-3xl cursor-pointer`} onClick={() => hanldeOperator("√")}>√</div>
                         </div>
                         <div className={`flex gap-4  mb-4 text-center`}>
-                            <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={clearScreen}>AC</div>
-                            <div className={`p-3 w-15 ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={backSpace}>⌫</div>
-                            <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeOperator("*")}>*</div>
-                            <div className={`p-3 w-15 ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeOperator("/")}>/</div>
+                            <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={clearScreen}>AC</div>
+                            <div className={`p-3 w-15 ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={backSpace}>⌫</div>
+                            <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeOperator("*")}>*</div>
+                            <div className={`p-3 w-15 ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeOperator("/")}>/</div>
                         </div>
                         <div className={`flex gap-6.25  mb-4 text-center`}>
                             <div>
                                 <div className={`flex gap-3  mb-4 text-center`}>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("7")}>7</div>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("8")}>8</div>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("9")}>9</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("7")}>7</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("8")}>8</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("9")}>9</div>
                                 </div>
                                 <div className={`flex gap-3  mb-4 text-center`}>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("4")}>4</div>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("5")}>5</div>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("6")}>6</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("4")}>4</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("5")}>5</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("6")}>6</div>
                                 </div>
                                 <div className={`flex gap-3  mb-4 text-center`}>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("1")}>1</div>
-                                    <div className={`p-3 w-15 ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("2")}>2</div>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("3")}>3</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("1")}>1</div>
+                                    <div className={`p-3 w-15 ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("2")}>2</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("3")}>3</div>
                                 </div>
                                 <div className={`flex gap-3  mb-4 text-center`}>
-                                    <div className={`p-3 w-33  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("0")}>0</div>
+                                    <div className={`p-3 w-33  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber("0")}>0</div>
 
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber(".")}>.</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeNumber(".")}>.</div>
                                 </div>
 
                             </div>
                             <div>
                                 <div className={`flex flex-col gap-4  mb-4 text-center`}>
-                                    <div className={`p-3 w-15  ${theme ? styles.colorD : styles.colorL } rounded-xl cursor-pointer`} onClick={() => hanldeOperator("-")}>-</div>
-                                    <div className={`p-3 w-15 h-20 ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeOperator("+")}>+</div>
-                                    <div className={`p-3 w-15 h-20 ${theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => handleEqual("=")}>=</div>
+                                    <div className={`p-3 w-15  ${!theme ? styles.colorD : styles.colorL } rounded-xl cursor-pointer`} onClick={() => hanldeOperator("-")}>-</div>
+                                    <div className={`p-3 w-15 h-20 ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => hanldeOperator("+")}>+</div>
+                                    <div className={`p-3 w-15 h-20 ${!theme ? styles.colorD : styles.colorL} rounded-xl cursor-pointer`} onClick={() => handleEqual("=")}>=</div>
                                 </div>
                             </div>
                         </div>
